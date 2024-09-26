0e3a70ea7f3ebed5ebdb27c2207fae94204cbe04
JKW301 <julien.khalifa.95@gmail.com>
2024-09-26 18:51:06 +0200

    ttt

--- links_2.c
+++ links_2.c
@@ -5,12 +5,14 @@
 struct node {
     int data;
     struct node* next;
+    // struct node* next means that next is a pointer to a node
 };
 
 // Change the passed in head pointer to be NULL
 // Uses a reference pointer to access the caller's memory
 void ChangeToNull(struct node** headRef) {
     *headRef = NULL; // use '*' to access the value of interest
+    // headref is a reference pointer to the caller's memory
 }
 
 void ChangeCaller() {
@@ -26,13 +28,17 @@ void ChangeCaller() {
     printf("Before ChangeToNull:\n");
     printf("head1: %p\n", (void*)head1);
     printf("head2: %p\n", (void*)head2);
+    // %p is used to print the pointer value
     
     ChangeToNull(&head1); // use '&' to compute and pass a pointer to
     ChangeToNull(&head2); // the value of interest
     
     printf("After ChangeToNull:\n");
     printf("head1: %p\n", (void*)head1);
+    printf("head1: %d\n", head1->data); //return the same value as before
     printf("head2: %p\n", (void*)head2);
+    // (void*)head1 is used to cast the pointer to a void pointer
+    // without this, the compiler will throw a warning
     
     // Free the allocated memory (though it's NULL now, it's good practice)
     free(head1);
